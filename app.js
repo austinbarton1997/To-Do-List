e76254ac6e51f120171ba659d8475694257f4439
Austin Barton <mail@austinbarton.dev>
2020-09-05 12:30:53 -0600

    CSS Update, Using EJS to generate list

--- app.js
+++ app.js
@@ -1,9 +1,33 @@
 const express = require("express");
 const bodyParser = require("body-parser");
+const { render } = require("ejs");
 const app = express();
+let items = [];
+
+app.set("view engine", "ejs");
+app.use(
+  bodyParser.urlencoded({
+    extended: true,
+  })
+);
+app.use(bodyParser.json());
+app.use(express.static("public"));
 
 app.get("/", (req, res) => {
-  res.send("test");
+  const today = new Date();
+  const options = {
+    weekday: "long",
+    day: "numeric",
+    month: "long",
+  };
+  const day = today.toLocaleDateString("en-us", options);
+  res.render("list", { kindOfDay: day, newListItem: items });
+});
+
+app.post("/", function (req, res) {
+  let item = req.body.newItem;
+  items.push(item);
+  res.redirect("/");
 });
 
 app.listen(3000, () => {
