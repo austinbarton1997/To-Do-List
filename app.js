4edeee36d7595bd55dcf193654a37e3a0dfa30cc
Austin Barton <mail@austinbarton.dev>
2020-09-07 09:55:39 -0600

    created custom date module.

--- app.js
+++ app.js
@@ -1,9 +1,10 @@
 const express = require("express");
 const bodyParser = require("body-parser");
 const { render } = require("ejs");
+const date = require(__dirname + "/date.js");
 const app = express();
-let items = [];
-let workItems = [];
+const items = [];
+const workItems = [];
 
 app.set("view engine", "ejs");
 app.use(
@@ -15,13 +16,7 @@ app.use(bodyParser.json());
 app.use(express.static("public"));
 
 app.get("/", (req, res) => {
-  const today = new Date();
-  const options = {
-    weekday: "long",
-    day: "numeric",
-    month: "long",
-  };
-  const day = today.toLocaleDateString("en-us", options);
+  const day = date.getDay();
   res.render("list", { listTitle: day, newListItem: items });
 });
 
@@ -34,7 +29,7 @@ app.get("/about", (req, res) => {
 });
 
 app.post("/", function (req, res) {
-  let item = req.body.newItem;
+  const item = req.body.newItem;
   console.log(req.body);
   if (req.body.list === "Work List") {
     workItems.push(item);
